fe730c0b9cdff0878c5da6533c6b099e6d88a283
Đặng Tuấn Thành <74081772+dangtuanthanh@users.noreply.github.com>
2025-03-07 10:38:21 +0700

    new upload

---
+++ build.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+var PORT = normalizePort(process.env.portListen || '3000');
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+const serverInstance = app.listen(PORT, () => {
+    console.log(`🌍 Running temp server on ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+    res.send("⏳ Server is under maintenance. Please wait...!");
+});
+async function startProcess() {
+    console.log("⏳ Waiting for 30 seconds before closing the server...");
+
+    setTimeout(() => {
+        console.log("🛑 Temporarily close server...");
+        serverInstance.close(() => {
+            console.log("✅ Server is temporarily down.");
+
+            console.log("🚀 Start main server...");
+            require("child_process").execSync("npm start", { stdio: "inherit" });
+        });
+    }, 30000); // 30 giây
+}
+
+startProcess();
